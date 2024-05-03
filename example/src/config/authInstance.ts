2524d4d7cd929bd60fe18b7fd75dffeb4f0c2628
Kenny Fukuya <kennyfukuya@gmail.com>
2024-05-03 15:00:43 -0300

    chore: removing sensible data

--- example/src/config/authInstance.ts
+++ example/src/config/authInstance.ts
@@ -1,7 +1,7 @@
 import { AloreAuth } from '@0xcarbon/alore-auth-sdk';
 
-export const apiKey = 'fmaymZJOlqI4HKxkOkXFcEdDG2K9p1KbkqjA0mA+tEM=';
-export const endpoint = 'https://alpha-api.bealore.com/v1';
+export const apiKey = 'abc123';
+export const endpoint = 'http://localhost:8000/api';
 
 export const aloreAuth = new AloreAuth(apiKey, {
   endpoint,
