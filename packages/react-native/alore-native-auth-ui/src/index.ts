6290ad06d0b7ca903905ec52385924f8d7cedff9
vitoalmeida <vitoralmeida.work@gmail.com>
2025-02-05 00:57:51 -0300

    chore: remove react-native packages

--- packages/react-native/alore-native-auth-ui/src/index.ts
+++
@@ -1,9 +0,0 @@
-import Auth, { AuthProps } from './components/Auth';
-import { SessionUser } from './machine/types';
-import useAuthService from './hooks/useAuthService';
-import { AuthProvider } from './components/AuthProvider';
-import './styles';
-
-export type { AuthProps, SessionUser };
-export { useAuthService, AuthProvider };
-export default Auth;
