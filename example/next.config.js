450b7e8d098b269316ed9270f7fe7cec78073879
vitoalmeida <vitoralmeida.work@gmail.com>
2025-02-05 10:34:45 -0300

    chore: move example folder

--- example/next.config.js
+++ example/next.config.js
@@ -19,12 +19,12 @@ module.exports = {
       test: /\.wasm$/,
       include: path.resolve(
         __dirname,
-        '../../node_modules/.pnpm/argon2-browser@1.18.0/node_modules/argon2-browser/dist',
+        '../node_modules/.pnpm/argon2-browser@1.18.0/node_modules/argon2-browser/dist',
       ),
       exclude: [
         path.resolve(
           __dirname,
-          '../../node_modules/.pnpm/file+..+..+dkls23-wasm+pkg/node_modules/@0xcarbon/dkls23-wasm',
+          '../node_modules/.pnpm/file+..+..+dkls23-wasm+pkg/node_modules/@0xcarbon/dkls23-wasm',
         ),
       ],
       loader: 'base64-loader',
