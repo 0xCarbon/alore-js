96316f135beebdf726e7b3e7e4f32950c2688876
vitoalmeida <vitoralmeida.work@gmail.com>
2025-02-05 10:53:11 -0300

    chore: change package names

--- example/src/config/authInstance.ts
+++ example/src/config/authInstance.ts
@@ -1,6 +1,6 @@
 import { AloreAuth } from '@alore/auth-react-sdk';
 
-export const apiKey = 'QcCVg5I0uNNyd5WncEwbUfntFlBV30PRdH7Q2/C+13U=';
+export const apiKey = 'TEST_API_KEY';
 export const endpoint = 'https://alpha-api.bealore.com/v1';
 
 export const aloreAuth = new AloreAuth(apiKey, { endpoint });
