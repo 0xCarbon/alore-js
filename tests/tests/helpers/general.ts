4e1816515aecec7607c9471466083227ef598b78
cadu0xcarbon <cadu@bealore.com>
2025-08-06 16:13:39 -0300

    chore: remove tests directory and related configurations

--- tests/tests/helpers/general.ts
+++
@@ -1,6 +0,0 @@
-import { AuthProviderConfig } from '@alore/auth-react-ui';
-import { Page } from '@playwright/test';
-
-export async function navigateTo(page: Page, url: string, params: AuthProviderConfig) {
-  await page.goto(`${url}?aloreConfigs=${JSON.stringify(params)}`);
-}
