a080e044afb551f362e4995680c17b2e466a18c5
cadu0xcarbon <cadu@bealore.com>
2025-08-06 11:18:24 -0300

    refactor: enhance Auth component with login/register callbacks

--- packages/alore-auth-ui/src/index.tsx
+++ packages/alore-auth-ui/src/index.tsx
@@ -5,7 +5,8 @@ import { AuthProvider } from './components/AuthProvider';
 import useAuthService from './hooks/useAuthService';
 import { LocalStorageAuthState } from './machine/LocalStorageAuthState';
 import { SessionUser } from './machine/types';
+import Login from './auth/Login';
+import Register from './auth/Register';
 
 export type { AuthProps, SessionUser, AuthProviderConfig, LocalStorageAuthState };
-export { useAuthService, AuthProvider };
-export default Auth;
+export { useAuthService, AuthProvider, Auth, Login, Register };
